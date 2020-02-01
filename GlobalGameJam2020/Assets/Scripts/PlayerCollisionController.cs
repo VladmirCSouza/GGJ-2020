a4844c8d4626fbf186a50a3da254356be117b6fb
Vladmir C Souza <vladmir.csouza@gmail.com>
2020-02-01 20:51:37 +0000

    Create de Player pieces collision logic (WIP)

---
+++ GlobalGameJam2020/Assets/Scripts/PlayerCollisionController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerPiece;
+    private float initialPlayerSize = 2.56f;
+    private float currentRightPiecePosition;
+    private float currentLeftPiecePosition;
+
+    private PlayerPiecesCollisionManager piecesCollisionManager;
+    void Start()
+    {
+        piecesCollisionManager = GameObject.FindGameObjectWithTag("PlayerPiecesCollisionManager").GetComponent<PlayerPiecesCollisionManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            IncreaseUp();
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            IncreaseRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            IncreaseLeft();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            piecesCollisionManager.AddPiece(collision.gameObject);
+            Vector2 collisionPoint = collision.GetContact(0).point;
+            //Debug.Log((int)(collisionPoint.x / initialPlayerSize));
+
+            //float difX = collision.transform.position.x - transform.position.x;
+            //float difY = collision.transform.position.y - transform.position.y;
+
+            //if (difY > initialPlayerSize)
+            //{
+            //    //Debug.Log("Bateu em Cima!");
+            //}
+            //if (difX < -initialPlayerSize)
+            //{
+            //    IncreaseLeft();
+            //}
+            //if (difX > initialPlayerSize)
+            //{
+            //    IncreaseRight();
+            //}
+
+            //Destroy(collision.gameObject);
+        }
+    }
+
+    private void IncreaseUp()
+    {
+        Vector3 piecePosition = transform.position + initialPlayerSize * Vector3.up;
+        Instantiate(playerPiece, piecePosition, transform.rotation, transform);
+    }
+
+    private void IncreaseRight()
+    {
+        currentRightPiecePosition += initialPlayerSize;
+        Vector3 piecePosition = transform.position + (currentRightPiecePosition * Vector3.right);
+        Instantiate(playerPiece, piecePosition, transform.rotation, transform);
+    }
+    private void IncreaseLeft()
+    {
+        currentLeftPiecePosition += initialPlayerSize;
+        Vector3 piecePosition = transform.position + (currentLeftPiecePosition * Vector3.left);
+        Instantiate(playerPiece, piecePosition, transform.rotation, transform);
+    }
+}
---
+++ GlobalGameJam2020/Assets/Scripts/PlayerPiecesCollisionManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPiecesCollisionManager : MonoBehaviour
+{
+    private List<GameObject> pieces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pieces = new List<GameObject>();
+    }
+
+    private void LateUpdate()
+    {
+        if (pieces.Count > 1)
+        {
+            Destroy(pieces[0]);
+            pieces.Clear();
+        }
+    }
+
+    public void AddPiece(GameObject piece)
+    {
+        Debug.Log(piece.name);
+        pieces.Add(piece);
+    }
+}
